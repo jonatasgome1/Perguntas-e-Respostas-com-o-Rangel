d5f891715b0a031838f425e9ca732d971309d239
jonatasgomes <jonatasg503@gmail.com>
2023-09-22 09:35:50 -0300

    teste de envio

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PerguntasRespostas/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PerguntasRespostas from './components/PerguntasRespostas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PerguntasRespostas
   }
 }
 </script>
---
+++ src/components/PerguntasRespostas.vue
@@ -0,0 +1,39 @@
+<template>  
+    <div>
+      <form @submit.prevent="gerarResposta">
+        <label for="pergunta">Faça uma pergunta:</label>
+        <input type="text" id="pergunta" v-model="question" required>
+        <button type="submit">Gerar Resposta</button>
+      </form>
+  
+      <div v-if="answer">
+        <p><strong>Sua Pergunta:</strong> {{ question }}</p>
+        <p><strong>Resposta:</strong> {{ answer }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        question: '',
+        answer: ''
+      };
+    },
+    methods: {
+      gerarResposta() {
+        const respostas = [
+          'Com certeza',
+          'É... não tenho certeza!',
+          'Claro que não',
+          'Acho difícil',
+          'Possivelmente não'
+        ];
+        const indiceAleatorio = Math.floor(Math.random() * respostas.length);
+        this.answer = respostas[indiceAleatorio];
+      }
+    }
+  };
+  </script>
+  
